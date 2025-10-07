20bd30204a8cfbecde45d21c0b063d10c43c44d2
FerDaniel01 <fernando.drinconruiz@gmail.com>
2025-10-06 22:31:06 -0500

    search-meal

---
+++ src/app/gifs/pages/search-meal/search-meal.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit  } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+@Component({
+  selector: 'app-search-meal',
+  imports: [],
+  templateUrl: './search-meal.html',
+  styles: ``
+})
+export class SearchMeal {
+
+}
