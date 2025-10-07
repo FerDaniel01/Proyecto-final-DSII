33a59647a00781de035c3044ae142ed0528fea44
FerDaniel01 <fernando.drinconruiz@gmail.com>
2025-10-06 21:52:43 -0500

    Inicio proyecto final

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+
+    production: true,
+    companyName: 'Gifs Test Fer',
+    companyName2: 'App',
+    companySlogan: 'Maneje tu gift',
+
+    apikey: '0dU3SqAi3Ofjixpww776MFkVeaUYfqre',
+    urlBase: 'https://api.giphy.com/v1'
+
+};
