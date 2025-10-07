f98348a2fca60d26dfb1501bcdfbce35282363f9
jdreyes <jdreyes.m07@gmail.com>
2025-10-06 22:05:33 -0500

    Cambio de slogan

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,9 @@
 export const environment = {
 
     production: true,
-    companyName: 'Meal Test Grupo 4',
+    companyName: 'Meal Grupo 4',
     companyName2: 'App',
-    companySlogan: 'Meal',
+    companySlogan: 'RESTAURANTE',
 
     apikey: '0dU3SqAi3Ofjixpww776MFkVeaUYfqre',
     urlBase: 'https://api.giphy.com/v1'
